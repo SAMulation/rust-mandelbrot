ad8f67c9572b2d6aabe7bdf943ae3707ec7c0e9b
Sam Sarsten <sam.sarsten@gmail.com>
2022-12-26 19:26:34 -0800

    Initial commit

---
+++ mandelbrot/target/debug/deps/mandelbrot-b2be06adf58beeac.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/Projects/repos/rust-mandelbrot/mandelbrot/target/debug/deps/mandelbrot-b2be06adf58beeac.rmeta: src/main.rs
+
+/Users/sam/Documents/Projects/repos/rust-mandelbrot/mandelbrot/target/debug/deps/mandelbrot-b2be06adf58beeac.d: src/main.rs
+
+src/main.rs:
---
+++ mandelbrot/target/debug/deps/mandelbrot-b6a66621c7ed00b0.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/Projects/repos/rust-mandelbrot/mandelbrot/target/debug/deps/mandelbrot-b6a66621c7ed00b0.rmeta: src/main.rs
+
+/Users/sam/Documents/Projects/repos/rust-mandelbrot/mandelbrot/target/debug/deps/mandelbrot-b6a66621c7ed00b0.d: src/main.rs
+
+src/main.rs:
