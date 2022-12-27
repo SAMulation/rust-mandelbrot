6cbfa9199e9801ea8dd2a15f592c783b848171d5
Sam Sarsten <sam.sarsten@gmail.com>
2022-12-26 20:09:25 -0800

    Working again

---
+++ mandelbrot/target/debug/deps/mandelbrot-d24d7af795a50107.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/Projects/repos/rust-mandelbrot/mandelbrot/target/debug/deps/mandelbrot-d24d7af795a50107: src/main.rs
+
+/Users/sam/Documents/Projects/repos/rust-mandelbrot/mandelbrot/target/debug/deps/mandelbrot-d24d7af795a50107.d: src/main.rs
+
+src/main.rs:
